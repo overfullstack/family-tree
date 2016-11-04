466f02c8e63620b6a487475753a3484d304e9283
Gopal S Akshintala <gopalakshintala@gmail.com>
2016-11-04 11:14:27 +0530

    License update

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2016, Gopal S Akshintala
- * This source code is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
- * http://creativecommons.org/licenses/by-sa/4.0/
- */
-
 import coreGraph.ConnectionEdgeTest;
 import coreGraph.FamilyGraphTest;
 import loader.FileLoaderServiceTest;
